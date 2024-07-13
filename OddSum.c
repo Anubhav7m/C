2786f3ab7b275ca13895aef2bd235ae633f27d25
Anubhav7m <anubhavmishra.x777@gmail.com>
2024-07-13 20:49:58 +0530

    C

--- OddSum.c
+++ OddSum.c
@@ -3,9 +3,20 @@
 
 int main(int argc, char const *argv[])
 {
-    int a,b,c;
-    printf("Enter 5 numbers:\n");
-    scanf("%d", &a,b,c);
-    if(a%2 == 0 && b%2==0 && c)
+    int num[5];
+    int sum = 0;
+    printf("Enter 5 numbers: \n");
+    for (int j = 0; j < 5; j++)
+    {
+        scanf("%d", &num[j]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        if (num[i] % 2 != 0)
+        {
+            sum += num[i];
+        }
+    }
+    printf("Sum of odd numbers is = %d\n", sum);
     return 0;
 }
